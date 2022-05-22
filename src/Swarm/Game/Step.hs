7140c41c7ec8981ee468bba9f89fa90d37f80ed2
Ondřej Šebek <xsebek@fi.muni.cz>
2022-05-22 11:36:29 +0200

    WIP: Find robots
    In challenges its impossible to know
    before-hand what the robot ID will be.
    Seems like we should be able to find it
    by its name though.

--- src/Swarm/Game/Step.hs
+++ src/Swarm/Game/Step.hs
@@ -206,6 +206,10 @@ updateEntityAt loc upd = zoomWorld (W.updateM @Int (W.locToCoords loc) upd)
 robotWithID :: (Has (State GameState) sig m) => RID -> m (Maybe Robot)
 robotWithID rid = use (robotMap . at rid)
 
+-- | Get the robot with a given name.
+robotWithName :: (Has (State GameState) sig m) => Text -> m (Maybe Robot)
+robotWithName rname = use (robotMap . to IM.elems . to (find $ \r -> r ^. robotName == rname))
+
 -- | Manhattan distance between world locations.
 manhattan :: V2 Int64 -> V2 Int64 -> Int64
 manhattan (V2 x1 y1) (V2 x2 y2) = abs (x1 - x2) + abs (y1 - y2)
@@ -968,6 +972,12 @@ execConst c vs s k = do
 
         -- Return the value returned by the hypothetical command.
         return $ Out v s k
+      _ -> badConst
+    Find -> case vs of
+      [VString rname] -> do
+        r <- robotWithName rname
+        let (found, robot) = maybe (False, VUnit) ((True,) . VRobot . view robotID) r
+        return $ Out (VInj found robot) s k
       _ -> badConst
     Say -> case vs of
       [VString msg] -> do
